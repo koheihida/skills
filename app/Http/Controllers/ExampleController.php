bac02b34e0404b45235078156e02e700ce957bfb
koheihida <88849254+koheihida@users.noreply.github.com>
2021-12-30 19:56:09 +0900

    example.bladeのファイル名誤字修正

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -8,6 +8,6 @@ class ExampleController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        return view('example');
     }
 }
--- resources/views/example.blade.php
+++ resources/views/example.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-
+<h1>サンプル</h1>
 </body>
 </html>
 
