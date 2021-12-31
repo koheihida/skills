9da5a3b59117fcc04f06465c2af243f8e13021e9
koheihida <88849254+koheihida@users.noreply.github.com>
2021-12-31 23:12:50 +0900

    action="/skill/create" の実装

--- resources/views/skill/new.blade.php
+++ resources/views/skill/new.blade.php
@@ -9,23 +9,16 @@
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200 flex justify-center">
 
-                        <form class="w-full max-w-sm">
+                        <form class="w-full max-w-sm" action="/skill/create" method="POST">
+                            @csrf
                             <div class="md:flex md:items-center mb-6">
-                                <div class="md:w-1/3">
-                                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-                                    ID
-                                    </label>
-                                    <div class="md:w-2/3">
-
-                                    </div>
-                                </div>
                                 <div class="md:w-1/3">
                                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
                                     スキル名
                                     </label>
                                 </div>
                                 <div class="md:w-2/3">
-                                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************">
+                                    <input  name="skill_name" class=" appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"  placeholder="">
                                 </div>
                             </div>
                             <div class="md:flex md:items-center mb-6">
@@ -35,14 +28,21 @@
                                     </label>
                                 </div>
                                 <div class="md:w-2/3">
-
+                                    <select  name="skill_status" class=" appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password">
+                                       @foreach (\App\Models\skill::SKILL_STATUS_OBJECT as $key => $skillName)
+                                           <option value="{{ $key }}">{{ $skillName }}</option>
+                                       @endforeach
+                                    </select>
                                 </div>
                             </div>
                             <div class="md:flex md:items-center">
                             <div class="md:w-1/3"></div>
                             <div class="md:w-2/3">
-                                <button onclick="location.href='/skill'" class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                                <button onclick="location.href='/skill'" class="shadow bg-gray-500 hover:bg-gray-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
                                 戻る
+                                </button>
+                                <button type="submit" class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" >
+                                登録
                                 </button>
                             </div>
                             </div>
