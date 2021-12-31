4d8603d38aeac9ceec140d48ea7498e29f0406b9
koheihida <88849254+koheihida@users.noreply.github.com>
2021-12-31 16:22:10 +0900

    DBの内容をindex.bladeに表示させる

--- resources/views/skill/index.blade.php
+++ resources/views/skill/index.blade.php
@@ -14,7 +14,24 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-
+                        <table class="table-auto">
+                            <thead>
+                              <tr>
+                                <th class="px-4 py-2">ID</th>
+                                <th class="px-4 py-2">スキル名</th>
+                                <th class="px-4 py-2">ステータス</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($skills as $skill)
+                                    <tr>
+                                    <td class="border px-4 py-2">{{ $skill->id }}</td>
+                                    <td class="border px-4 py-2">{{ $skill->skill_name}}</td>
+                                    <td class="border px-4 py-2">{{ $skill->skill_status}}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                          </table>
                     </div>
                 </div>
             </div>
