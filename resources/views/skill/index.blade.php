f8dd441d233ebda95e9dfe8c26ccc0dcd0f24a5d
koheihida <88849254+koheihida@users.noreply.github.com>
2021-12-31 21:54:22 +0900

    新規作成ボタンの実装

--- resources/views/skill/index.blade.php
+++ resources/views/skill/index.blade.php
@@ -14,6 +14,9 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
+                        <button onclick="location.href='/skill/new'" class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                            新規作成
+                        </button>
                         <table class="table-auto">
                             <thead>
                               <tr>
